1a541b5a12bb53c7a3df246fb0feb269e4c1848c
Patrick Boivin <pboivin@gmail.com>
2020-12-21 08:41:09 -0500

    Cleanup

--- src/utils/throttle.js
+++ src/utils/throttle.js
@@ -1,7 +1,7 @@
 /**
  * Reference: https://stackoverflow.com/questions/27078285/simple-throttle-in-js
  */
-export default function throttle (callback, limit) {
+export default function throttle(callback, limit) {
     let waiting = false;
     return function () {
         if (!waiting) {
