578cd74a66b601a3ad10f6aefc80cfc1b03542e7
Patrick Boivin <pboivin@gmail.com>
2021-03-11 19:17:20 -0500

    Add scrollContainer option

--- src/modules/scroll-direction.js
+++ src/modules/scroll-direction.js
@@ -3,6 +3,7 @@
 import { throttle } from "../utils/event";
 
 const DEFAULT_CONFIG = {
+    scrollContainer: null,
     throttleTime: 10,
     up: () => {},
     down: () => {},
@@ -11,28 +12,27 @@ const DEFAULT_CONFIG = {
 export default class ScrollDirection {
     constructor(options={}) {
         this.config = Object.assign({}, DEFAULT_CONFIG, options);
+        this.listener = this.config.scrollContainer || window;
+        this.container = this.config.scrollContainer || document.body;
         this.previousScrollTop = -1;
-        this.scrollTop = document.body.scrollTop;
 
         this.initScrollListener();
     }
 
     destroy() {
-        window.removeEventListener('scroll', this.onScroll);
+        this.listener.removeEventListener('scroll', this.onScroll);
     }
 
     initScrollListener() {
         this.onScroll = throttle((e) => {
-            this.scrollTop = document.body.scrollTop;
-
-            if (this.scrollTop > this.previousScrollTop) {
+            if (this.container.scrollTop > this.previousScrollTop) {
                 this.config.down();
             } else {
                 this.config.up();
             }
-            this.previousScrollTop = this.scrollTop;
+            this.previousScrollTop = this.container.scrollTop;
         }, this.config.throttleTime);
 
-        window.addEventListener('scroll', this.onScroll);
+        this.listener.addEventListener('scroll', this.onScroll);
     }
 }
