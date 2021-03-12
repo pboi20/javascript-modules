4d55f848ae3bccedb3a4e6b3da69d8c70dcf71a6
Patrick Boivin <pboivin@gmail.com>
2021-03-11 19:22:37 -0500

    Use elementOrSelector

--- src/modules/scroll-direction.js
+++ src/modules/scroll-direction.js
@@ -1,5 +1,6 @@
 /*! scroll-direction.js | (c) 2021 Patrick Boivin | MIT License | https://github.com/pboi20/javascript-modules */
 
+import { elementOrSelector } from "../utils/element";
 import { throttle } from "../utils/event";
 
 const DEFAULT_CONFIG = {
@@ -12,8 +13,8 @@ const DEFAULT_CONFIG = {
 export default class ScrollDirection {
     constructor(options={}) {
         this.config = Object.assign({}, DEFAULT_CONFIG, options);
-        this.listener = this.config.scrollContainer || window;
-        this.container = this.config.scrollContainer || document.body;
+        this.listener = elementOrSelector(this.config.scrollContainer || window);
+        this.container = elementOrSelector(this.config.scrollContainer || document.body);
         this.previousScrollTop = -1;
 
         this.initScrollListener();
