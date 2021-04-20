040434cc2b27591c2317c5f57a6efba386d503e2
Patrick Boivin <pboivin@gmail.com>
2021-04-20 09:25:08 -0400

    Update viewport utils

--- src/utils/viewport.js
+++ src/utils/viewport.js
@@ -1,7 +1,25 @@
+/**
+ * Reference: https://stackoverflow.com/a/8876069
+ */
 export function getWidth() {
-    return Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  const doc = document.documentElement;
+  return Math.max(doc.clientWidth, window.innerWidth || 0);
 }
 
 export function getHeight() {
-    return Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  const doc = document.documentElement;
+  return Math.max(doc.clientHeight, window.innerHeight || 0);
+}
+
+/**
+ * Reference: https://stackoverflow.com/a/3464890
+ */
+export function getScrollY() {
+  const doc = document.documentElement;
+  return (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+}
+
+export function getScrollX() {
+  const doc = document.documentElement;
+  return (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0);
 }
